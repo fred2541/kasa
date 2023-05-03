045098553c05df87fe2407e97c958df5b261a2ad
fred2541 <fredionze@free.fr>
2023-05-03 10:11:01 +0200

    Change on routeur to correct the Layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import PublicRouter from './routeur/index'
 
-import Header from "./components/header";
-import Footer from "./components/footer";
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
         <PublicRouter />
-        <Footer />
-      </BrowserRouter>
     </div>
   );
 };
--- src/routeur/index.jsx
+++ src/routeur/index.jsx
@@ -8,17 +8,25 @@ import FicheLogement from "../pages/fiche-logement";
 import APropos from "../pages/a-propos";
 import Error404 from "../pages/404";
 
+import Header from "../components/header";
+import Footer from "../components/footer";
+
 
 const PublicRouter = () => {
  
     return (
+      <Router>
+        <Header />
         <Routes>
           <Route exact path="/" Component={Home}></Route>
           <Route path="/logement" Component={FicheLogement}></Route>
           <Route path="/about" Component={APropos}></Route>
           <Route path="*" Component={Error404}></Route>
+          <Route element={<Footer />} />
         </Routes>
-    )
+        <Footer />
+      </Router>
+    );
   }
   
 export default PublicRouter
